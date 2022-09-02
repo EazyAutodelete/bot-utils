c809b4bd162a4bd1180950e674724988301c784d
Qreepex <65496825+Qreepex@users.noreply.github.com>
2022-09-02 16:08:00 +0200

    fix(exports): fix exports of new functions

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export { default as msToDate } from "./utils/msToDate";
 export { default as msToDuration } from "./utils/msToDuration";
 export { default as stringToMs } from "./utils/stringToMs";
 export { default as getColor } from "./utils/getColor";
+export { default as uppercaseFirst } from "./utils/uppercaseFirst";
+export { default as randomInt } from "./utils/randomInt";
